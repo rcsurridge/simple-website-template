2e0a33946f14770b1148d4b3c2961f24d36c5cf6
Bobby Surridge <rcsurridge@uchicago.edu>
2023-11-29 18:24:36 -0600

    project changes

--- script.js
+++ script.js
@@ -29,7 +29,7 @@
 })(jQuery);
 
 /* Some random colors */
-const colors = ["#4CAF50", "#66BB6A", "#004B60", "#AAAAAA"];
+const colors = ["#558776", "#A3A847", "#7F8B52", "#064E76", "#071A52"];
 
 const numBalls = 50;
 const balls = [];
